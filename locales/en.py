2567497d1d066afa5877fd2945a1f4f58aff3ca3
NightStrang6r <47156355+NightStrang6r@users.noreply.github.com>
2026-09-08 13:42:43 +0200

    🧹 Убрано чужое название из текстов для Telegram
    adv_description в ru и uk уходил в публичное описание бота с названием
    прежней сборки, набранным математическим курсивом; там же были примеры
    водяного знака в 12 начертаниях. ASCII-поиск их не находил.

--- locales/en.py
+++ locales/en.py
@@ -265,13 +265,13 @@ proxy_adding_error = "❌ There was an error while adding the proxy."
 proxy_undeletable = "❌ This proxy cannot be deleted as it is currently in use."
 
 act_edit_watermark = "Enter a new watermark text. For example:\n{}\n" \
-                     "<code>𝓕𝓾𝓷𝓟𝓪𝔂 𝓒𝓪𝓻𝓭𝓲𝓷𝓪𝓵</code>\n" \
-                     "<code>𝔽𝕦𝕟ℙ𝕒𝕪 ℂ𝕒𝕣𝕕𝕚𝕟𝕒𝕝</code>\n<code>ＦｕｎＰａｙ Ｃａｒｄｉｎａｌ</code>\n" \
-                     "<code>ꜰᴜɴᴘᴀʏ ᴄᴀʀᴅɪɴᴀʟ</code>\n<code>🄵🅄🄽🄿🄰🅈 🄲🄰🅁🄳🄸🄽🄰🄻</code>\n" \
-                     "<code>ⒻⓤⓝⓅⓐⓨ Ⓒⓐⓡⓓⓘⓝⓐⓛ</code>\n<code>𝐅𝐮𝐧𝐏𝐚𝐲 𝐂𝐚𝐫𝐝𝐢𝐧𝐚𝐥</code>\n" \
-                     "<code>𝗙𝘂𝗻𝗣𝗮𝘆 𝗖𝗮𝗿𝗱𝗶𝗻𝗮𝗹</code>\n<code>𝘍𝘶𝘯𝘗𝘢𝘺 𝘊𝘢𝘳𝘥𝘪𝘯𝘢𝘭</code>\n" \
-                     "<code>𝙁𝙪𝙣𝙋𝙖𝙮 𝘾𝙖𝙧𝙙𝙞𝙣𝙖𝙡</code>\n<code>𝙵𝚞𝚗𝙿𝚊𝚢 𝙲𝚊𝚛𝚍𝚒𝚗𝚊𝚕</code>\n" \
-                     "<code>ᖴᑌᑎᑭᗩY ᑕᗩᖇᗪIᑎᗩᒪ</code>\n" \
+                     "<code>𝓕𝓾𝓷𝓟𝓪𝔂 𝓥𝓮𝓻𝓽𝓮𝔁</code>\n" \
+                     "<code>𝔽𝕦𝕟ℙ𝕒𝕪 𝕍𝕖𝕣𝕥𝕖𝕩</code>\n<code>ＦｕｎＰａｙ Ｖｅｒｔｅｘ</code>\n" \
+                     "<code>ꜰᴜɴᴘᴀʏ ᴠᴇʀᴛᴇx</code>\n<code>🄵🅄🄽🄿🄰🅈 🅅🄴🅁🅃🄴🅇</code>\n" \
+                     "<code>ⒻⓤⓝⓅⓐⓨ Ⓥⓔⓡⓣⓔⓧ</code>\n<code>𝐅𝐮𝐧𝐏𝐚𝐲 𝐕𝐞𝐫𝐭𝐞𝐱</code>\n" \
+                     "<code>𝗙𝘂𝗻𝗣𝗮𝘆 𝗩𝗲𝗿𝘁𝗲𝘅</code>\n<code>𝘍𝘶𝘯𝘗𝘢𝘺 𝘝𝘦𝘳𝘵𝘦𝘹</code>\n" \
+                     "<code>𝙁𝙪𝙣𝙋𝙖𝙮 𝙑𝙚𝙧𝙩𝙚𝙭</code>\n<code>𝙵𝚞𝚗𝙿𝚊𝚢 𝚅𝚎𝚛𝚝𝚎𝚡</code>\n" \
+                     "<code>ᖴᑌᑎᑭᗩY ᐯᕮᖇTᕮ᙭</code>\n" \
                      "<code>FunPay Vertex</code>\n<code>[FunPay / Vertex]</code>\n" \
                      "<code>🤖</code>\n<code>🐦</code>\n\n" \
                      "You can tap on the examples to copy and edit them to your liking.\nNote that on FunPay, the emoji " \
--- locales/ru.py
+++ locales/ru.py
@@ -266,13 +266,13 @@ proxy_adding_error = "❌ Во время добавления прокси пр
 proxy_undeletable = "❌ Эту прокси невозможно удалить, т.к. она используется сейчас."
 
 act_edit_watermark = "Введи новый текст водяного знака. Например:\n{}\n" \
-                     "<code>𝓕𝓾𝓷𝓟𝓪𝔂 𝓒𝓪𝓻𝓭𝓲𝓷𝓪𝓵</code>\n" \
-                     "<code>𝔽𝕦𝕟ℙ𝕒𝕪 ℂ𝕒𝕣𝕕𝕚𝕟𝕒𝕝</code>\n<code>ＦｕｎＰａｙ Ｃａｒｄｉｎａｌ</code>\n" \
-                     "<code>ꜰᴜɴᴘᴀʏ ᴄᴀʀᴅɪɴᴀʟ</code>\n<code>🄵🅄🄽🄿🄰🅈 🄲🄰🅁🄳🄸🄽🄰🄻</code>\n" \
-                     "<code>ⒻⓤⓝⓅⓐⓨ Ⓒⓐⓡⓓⓘⓝⓐⓛ</code>\n<code>𝐅𝐮𝐧𝐏𝐚𝐲 𝐂𝐚𝐫𝐝𝐢𝐧𝐚𝐥</code>\n" \
-                     "<code>𝗙𝘂𝗻𝗣𝗮𝘆 𝗖𝗮𝗿𝗱𝗶𝗻𝗮𝗹</code>\n<code>𝘍𝘶𝘯𝘗𝘢𝘺 𝘊𝘢𝘳𝘥𝘪𝘯𝘢𝘭</code>\n" \
-                     "<code>𝙁𝙪𝙣𝙋𝙖𝙮 𝘾𝙖𝙧𝙙𝙞𝙣𝙖𝙡</code>\n<code>𝙵𝚞𝚗𝙿𝚊𝚢 𝙲𝚊𝚛𝚍𝚒𝚗𝚊𝚕</code>\n" \
-                     "<code>ᖴᑌᑎᑭᗩY ᑕᗩᖇᗪIᑎᗩᒪ</code>\n" \
+                     "<code>𝓕𝓾𝓷𝓟𝓪𝔂 𝓥𝓮𝓻𝓽𝓮𝔁</code>\n" \
+                     "<code>𝔽𝕦𝕟ℙ𝕒𝕪 𝕍𝕖𝕣𝕥𝕖𝕩</code>\n<code>ＦｕｎＰａｙ Ｖｅｒｔｅｘ</code>\n" \
+                     "<code>ꜰᴜɴᴘᴀʏ ᴠᴇʀᴛᴇx</code>\n<code>🄵🅄🄽🄿🄰🅈 🅅🄴🅁🅃🄴🅇</code>\n" \
+                     "<code>ⒻⓤⓝⓅⓐⓨ Ⓥⓔⓡⓣⓔⓧ</code>\n<code>𝐅𝐮𝐧𝐏𝐚𝐲 𝐕𝐞𝐫𝐭𝐞𝐱</code>\n" \
+                     "<code>𝗙𝘂𝗻𝗣𝗮𝘆 𝗩𝗲𝗿𝘁𝗲𝘅</code>\n<code>𝘍𝘶𝘯𝘗𝘢𝘺 𝘝𝘦𝘳𝘵𝘦𝘹</code>\n" \
+                     "<code>𝙁𝙪𝙣𝙋𝙖𝙮 𝙑𝙚𝙧𝙩𝙚𝙭</code>\n<code>𝙵𝚞𝚗𝙿𝚊𝚢 𝚅𝚎𝚛𝚝𝚎𝚡</code>\n" \
+                     "<code>ᖴᑌᑎᑭᗩY ᐯᕮᖇTᕮ᙭</code>\n" \
                      "<code>FunPay Vertex</code>\n<code>[FunPay / Vertex]</code>\n" \
                      "<code>🤖</code>\n<code>🐦</code>\n\n" \
                      "Примеры можно скопировать нажатием и отредактировать по своему желанию.\nУчти, что на FunPay эмодзи " \
@@ -420,7 +420,7 @@ pl_new = "Отправь мне плагин.\n\n<b>⚠️ ВНИМАНИЕ! З�
 
 au_user_settings = "Настройки для пользователя {}"
 adv_fpv = "😎 FunPay Vertex - лучший бот для FunPay"
-adv_description = """🐦 𝑭𝒖𝒏𝑷𝒂𝒚 𝑪𝒂𝒓𝒅𝒊𝒏𝒂𝒍 v{}🐦
+adv_description = """🐦 𝑭𝒖𝒏𝑷𝒂𝒚 𝑽𝒆𝒓𝒕𝒆𝒙 v{}🐦
 
 🤖 Автовыдача товаров
 🚀 Автоподнятие лотов
